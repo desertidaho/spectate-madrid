c43f3fcce0c69a2552a8afc4261bfe8f920a1105
Brett Wilcox <wilcox.brett@gmail.com>
2019-04-22 18:45:32 -0600

    Post npm build.

---
+++ client/dist/precache-manifest.4f30a2979402f6c38a3d2e6916ba17c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfc19966efa6ab29ff8a",
+    "url": "/css/app.d3b47c84.css"
+  },
+  {
+    "revision": "cfc19966efa6ab29ff8a",
+    "url": "/js/app.a6d6bd6d.js"
+  },
+  {
+    "revision": "02de75655e3c3f4dae17",
+    "url": "/css/chunk-vendors.b89365ac.css"
+  },
+  {
+    "revision": "02de75655e3c3f4dae17",
+    "url": "/js/chunk-vendors.5d398483.js"
+  },
+  {
+    "revision": "300b520c773f538a3f806a1f9abc73a5",
+    "url": "/img/spectate-logo.300b520c.png"
+  },
+  {
+    "revision": "6d8ba967cd18cc0b2a0b386708da5e32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
